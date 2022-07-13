a23c84e33d496a965e185fa70d853918f2df09fc
cokecider <109188240+cokecider@users.noreply.github.com>
2022-07-13 10:16:31 +0900

    Delete 214.베어링반력프로그램 directory

--- 214.베어링반력프로그램/sineFit201208/BasicTest.m
+++
@@ -1,7 +0,0 @@
-    x=t_fil_cut_dwn(:,1);
-    y=data_fil_cut_dwn(:,1);%Sine + noise
-    tic;
-    [SineP]=sineFit(x,y) %Offset, amplitude, frquency, phase, MSE
-    toc
-%uncomment following line if you want to save y=f(x) and run it sineFitDemo
-% save('xy.mat','x','y');
